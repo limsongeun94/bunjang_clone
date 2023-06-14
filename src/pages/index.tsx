006387ca74470daac7de25c8f7641b6916a9872d
limsongeun94 <cocorosaya@naver.com>
2023-06-14 23:25:47 +0900

    product thumbnail

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,22 @@ export default ({ data }: any) => {
 
   return (
     <div className="w-[1024px] mx-auto">
-      {/* <div className="w-full h-[30px] bg-black" /> */}
       <Carousel />
+      <section className="pt-[56px]">
+        <h2 className="text-2xl mb-6">오늘의 상품 추천</h2>
+        <div className="flex flex-wrap">
+          <div className="flex-item-propduct w-[196px] h-[276px] mr-[11px] mb-[11px] border-[1px] border-[#eeeeee]">
+            <div className="w-[194px] h-[194px] border-b-[1px] border-[#eeeeee]"></div>
+            <div className="w-[194px] h-[80px] py-[15px] px-[10px] flex flex-col justify-between">
+              <div className="text-sm">상품제목</div>
+              <div className="flex justify-between items-center">
+                <div className="text-base font-semibold after-won">4,000</div>
+                <div className="text-xs text-[#888888]">3시간 전</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
