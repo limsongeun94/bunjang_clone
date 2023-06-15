dc7d29a0a1971220c3b4097689b1cf68c677d449
limsongeun94 <cocorosaya@naver.com>
2023-06-15 23:33:42 +0900

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,57 +17,87 @@ const Footer = () => {
         <div className="grid grid-col-2">
           <div className="col-start-1 col-end-2 row-start-1 row-end-2">
             <div>
-              <p>번개장터(주) 사업자정보</p>
-              <p>대표이사 : 최재화, 강승현 | 개인정보보호책임자 : 박병성</p>
-              <p>
-                사업자등록번호 : 113-86-45836 | 통신판매업신고 :
-                2019-서울서초-1126
-              </p>
-              <p>호스팅서비스 제공자 : Amazon Web Services (AWS)</p>
-              <p>EMAIL : help@bunjang.co.kr | FAX : 02-598-8241</p>
-              <p>
-                주소 : 서울특별시 서초구 서초대로 38길 12, 7, 10층(서초동,
-                마제스타시티, 힐스테이트 서리풀)
-              </p>
-              <p>사업자정보 확인</p>
+              <div className="text-sm font-bold text-[#666666]">
+                번개장터(주) 사업자정보
+              </div>
+              <div className="mt-[8px] text-[12px] text-[#7f7f7f] leading-loose">
+                <p className="mb-0">
+                  대표이사 : 최재화, 강승현 | 개인정보보호책임자 : 박병성
+                </p>
+                <p className="mb-0">
+                  사업자등록번호 : 113-86-45836 | 통신판매업신고 :
+                  2019-서울서초-1126
+                </p>
+                <p className="mb-0">
+                  호스팅서비스 제공자 : Amazon Web Services (AWS)
+                </p>
+                <p className="mb-0">
+                  EMAIL : help@bunjang.co.kr | FAX : 02-598-8241
+                </p>
+                <p className="mb-0">
+                  주소 : 서울특별시 서초구 서초대로 38길 12, 7, 10층(서초동,
+                  마제스타시티, 힐스테이트 서리풀)
+                </p>
+                <p className="mb-0">사업자정보 확인</p>
+              </div>
             </div>
-            <div>
-              <p>번개장터(주)더현대서울점 | 최재화, 강승현 | 365-85-01581</p>
-              <p>서울특별시 영등포구 여의대로 108, 지하2층(여의도동, 파크원)</p>
-              <p>번개장터(주)코엑스점 | 최재화, 강승현 | 142-85-27412</p>
-              <p>
+            <div className="mt-[24px] text-[12px] text-[#7f7f7f] leading-loose">
+              <div className="mt-[8px]">
+                <b>번개장터(주)더현대서울점</b> | 최재화, 강승현 | 365-85-01581
+                <br />
+                서울특별시 영등포구 여의대로 108, 지하2층(여의도동, 파크원)
+              </div>
+              <div className="mt-[8px]">
+                <b>번개장터(주)코엑스점</b> | 최재화, 강승현 | 142-85-27412
+                <br />
                 서울특별시 강남구 영동대로 513, 쇼핑몰동 B1층 C102호(삼성동,
                 코엑스)
-              </p>
-              <p>번개장터(주)센터필드점 | 최재화, 강승현 | 808-85-01905</p>
-              <p>
+              </div>
+              <div className="mt-[8px]">
+                <b>번개장터(주)센터필드점</b> | 최재화, 강승현 | 808-85-01905
+                <br />
                 서울특별시 강남구 테헤란로 231, 쇼핑몰동 1층
                 W124호(역삼동)(역삼동, 센터필드)
-              </p>
+              </div>
             </div>
           </div>
           <div className="col-start-2 col-end-3 row-start-1 row-end-2">
-            <div>
-              <p>고객센터</p>
-              <p>1670-2910</p>
-              <p>
+            <div className="text-[#666666]">
+              <p className="text-[14px] font-bold  mb-0">고객센터</p>
+              <div className="mt-[8px] text-xs leading-loose">
+                <strong className="text-[25px] leading-none">1670-2910</strong>
+                <br />
                 운영시간 9시 - 18시 (주말/공휴일 휴무, 점심시간 12시 - 13시)
-              </p>
-              <div className="flex">
-                <div>공지사항</div>
-                <div>1:1 문의하기</div>
-                <div>자주 묻는 질문</div>
+                <br />
+                <div className="flex">
+                  <div className="mr-[10px] underline cursor-pointer">
+                    공지사항
+                  </div>
+                  <div className="mr-[10px] underline cursor-pointer">
+                    1:1 문의하기
+                  </div>
+                  <div className="mr-[10px] underline cursor-pointer">
+                    자주 묻는 질문
+                  </div>
+                </div>
               </div>
             </div>
-            <div>
-              <p>우리은행 채무지급보증 안내</p>
-              <p>
+            <div className="mt-[40px] text-[#666666]">
+              <p className="text-[14px] font-bold  mb-0">
+                우리은행 채무지급보증 안내
+              </p>
+              <div className="mt-[8px] text-[12px] leading-loose">
                 번개장터㈜는 회사가 직접 판매하는 상품에 한하여, 고객님의 현금
                 결제 금액에 대해 우리은행과 채무지급보증 계약을 체결하여
                 안전거래를 보장하고 있습니다.
-              </p>
-              <p>서비스 가입사실 확인</p>
-              <p>Ⓒ Bungaejangter. Inc All rights reserved.</p>
+                <br />
+                <p className="underline cursor-pointer mb-0">
+                  서비스 가입사실 확인
+                </p>
+                <p className="mt-[8px] mb-0">
+                  Ⓒ Bungaejangter. Inc All rights reserved.
+                </p>
+              </div>
             </div>
           </div>
           <hr className="col-start-1 col-end-3 row-start-2 row-end-3" />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,41 @@
 import "@/styles/globals.css";
+import { Noto_Sans_KR } from "next/font/google";
 import { Inter } from "next/font/google";
 import Layout from "@/components/Layout";
 
+export const cls = (...classnames: string[]) => {
+  return classnames.join(" ");
+};
+
+const notoSansKr = Noto_Sans_KR({
+  // preload: true, 기본값
+  subsets: ["latin"], // 또는 preload: false
+  weight: ["100", "400", "700", "900"], // 가변 폰트가 아닌 경우, 사용할 fontWeight 배열
+});
+
 const inter = Inter({ subsets: ["latin"] });
+
+// export default ({ Component, pageProps }: any) => {
+//   return (
+//     <div className={Noto_Sans_KR.className}>
+//       <Layout>
+//         <Component {...pageProps} />
+//       </Layout>
+//     </div>
+//   );
+// };
 
 export default ({ Component, pageProps }: any) => {
   return (
-    <>
+    <div>
       <Layout>
         <style jsx global>{`
           html {
-            font-family: ${inter.style.fontFamily};
+            font-family: ${notoSansKr.style.fontFamily};
           }
         `}</style>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </div>
   );
 };
