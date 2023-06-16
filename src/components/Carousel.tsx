c5da882a30800211fd06fcfbf535e5b51f3ecf5b
limsongeun94 <cocorosaya@naver.com>
2023-06-16 21:01:59 +0900

    carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,17 +3,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
-import type { Banner } from '@/interface';
+import type { Banner } from "@/interface";
 
-// type Banner = { url: string }[];
 interface CarouselProps {
-  banner_img: Banner[]
+  banner_img: Banner[];
 }
 
 function ControlledCarousel({ banner_img }: CarouselProps) {
   const [index, setIndex] = useState(0);
 
-  const handleSelect = (selectedIndex: any) => {
+  const handleSelect = (selectedIndex: number) => {
     setIndex(selectedIndex);
   };
 
