c7785f8bc5786aec0182f3ef9d46ee2ccc6fb636
limsongeun94 <cocorosaya@naver.com>
2023-07-15 15:06:26 +0900

    floating menu

--- src/components/FloatingMenu.tsx
+++ src/components/FloatingMenu.tsx
@@ -1,6 +1,25 @@
+import { useState, useEffect } from "react";
+
 const FlotingMenu = () => {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const resizeListener = () => {
+      setInnerWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", resizeListener);
+  });
+
+  console.log(innerWidth);
+
   return (
-    <div className="w-[90px] box-border leading-none fixed top-[102px] right-[calc(50%-617px)]">
+    <div
+      className={`w-[90px] box-border leading-none fixed top-[102px] ${
+        innerWidth > 1280
+          ? "right-[calc(50%-617px)]"
+          : "right-[calc(640px-617px)]"
+      } `}
+    >
       <div className="border-[#666666] border-[1px]  p-[10px] mb-[6px]">
         <div className="text-xs font-semibold text-[#666666] text-center mb-[8px]  leading-none">
           찜한상품
