92b46ae26dfb6c7d3bc6fea022528b78e82ead35
limsongeun94 <cocorosaya@naver.com>
2023-08-03 15:07:00 +0900

    input component - maxlength 설정하면 입력 안 되는 문제 해결

--- src/components/MyInput.tsx
+++ src/components/MyInput.tsx
@@ -9,11 +9,19 @@ interface MyInputProps {
   maxlenght?: number;
   inputType: string;
   onChange?: (val: string) => void;
+  name?: string;
 }
 
 const MyInput = (props: MyInputProps) => {
-  const { children, placeholder, textcenter, maxlenght, inputType, onChange } =
-    props;
+  const {
+    children,
+    placeholder,
+    textcenter,
+    maxlenght,
+    inputType,
+    onChange,
+    name,
+  } = props;
   const onchangehandler = (value: string): void => {
     if (onChange) {
       onChange(value);
@@ -21,8 +29,10 @@ const MyInput = (props: MyInputProps) => {
   };
 
   const setMaxLength = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length > e.target.maxLength) {
-      e.target.value = e.target.value.slice(0, e.target.maxLength);
+    if (maxlenght) {
+      if (e.target.value.length > e.target.maxLength) {
+        e.target.value = e.target.value.slice(0, e.target.maxLength);
+      }
     }
   };
 
@@ -30,6 +40,7 @@ const MyInput = (props: MyInputProps) => {
     <div className="field">
       <label>
         <input
+          name={name}
           className={`${textcenter} field_input`}
           type={inputType}
           placeholder={placeholder}
