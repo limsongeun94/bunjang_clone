d496e71d712435d6dbc9a2e22e3d1e2cd1c30557
limsongeun94 <cocorosaya@naver.com>
2023-08-03 15:16:57 +0900

    top header - 로그인 로그아웃 버튼 숨기기 보이기

--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -1,8 +1,22 @@
+import type { User } from "@/interface";
+import { useEffect, useState } from "react";
+
 interface LayoutProps {
   setLoginModal: React.Dispatch<React.SetStateAction<boolean>>;
+  user: User;
 }
 
-const TopHeader = ({ setLoginModal }: LayoutProps) => {
+const TopHeader = ({ setLoginModal, user }: LayoutProps) => {
+  const [loginState, setLoginState] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      setLoginState(true);
+    } else {
+      setLoginState(false);
+    }
+  }, [user]);
+
   return (
     <div className=" w-[1024px] mx-auto  bg-white h-auto">
       <div className="flex justify-between items-center h-[40px] border-b-[1px]">
@@ -19,11 +33,22 @@ const TopHeader = ({ setLoginModal }: LayoutProps) => {
         <div className="flex">
           <div
             onClick={() => setLoginModal(true)}
-            className="text-[13px] px-[15px] cursor-pointer"
+            className={`${
+              loginState ? "hidden" : ""
+            } text-[13px] px-[15px] cursor-pointer`}
           >
             로그인/회원가입
           </div>
           <div className="text-[13px] px-[15px] cursor-pointer">내 상점</div>
+          <form
+            method="POST"
+            action="/api/logout"
+            className={`${
+              loginState ? "" : "hidden"
+            } text-[13px] px-[15px] cursor-pointer`}
+          >
+            <button type="submit">로그아웃</button>
+          </form>
         </div>
       </div>
     </div>
