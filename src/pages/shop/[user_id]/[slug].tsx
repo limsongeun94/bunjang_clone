95d2478e7a6864b5902fbd3e144a97603cb277f2
limsongeun94 <cocorosaya@naver.com>
2023-08-14 17:00:34 +0900

    ...

--- src/pages/shop/[user_id]/[slug].tsx
+++ src/pages/shop/[user_id]/[slug].tsx
@@ -3,6 +3,8 @@ import axios from "@/libs/axios";
 import type { Banner, Category, Product, User } from "@/interface";
 import { withIronSessionSsr } from "iron-session/next";
 import { ironSessionOptions } from "@/libs/session";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 interface IndexProps {
   data: {
@@ -14,6 +16,24 @@ interface IndexProps {
 }
 
 export default ({ data, user }: IndexProps) => {
+  const router = useRouter();
+  const [routerType, setRouterType] = useState("");
+  useEffect(() => {
+    if (router.query.slug === "products") {
+      setRouterType("products");
+    } else if (router.query.slug === "reviews") {
+      setRouterType("reviews");
+    } else if (router.query.slug === "favorites") {
+      setRouterType("favorites");
+    } else if (router.query.slug === "followings") {
+      setRouterType("followings");
+    } else if (router.query.slug === "followers") {
+      setRouterType("followers");
+    }
+  });
+
+  const [categoriesFilter, setCategoriesFilter] = useState(false);
+
   return (
     <MainLayout categories={data.categories} user={user}>
       <div className="w-[1024px] mx-auto mt-[30px]">
@@ -97,6 +117,99 @@ export default ({ data, user }: IndexProps) => {
               <button className="h-[20px] flex items-center px-[5px] text-[#888888] border-[1px] border-[#eeeeee] text-[11px]">
                 소개글 수정
               </button>
+            </div>
+          </div>
+        </div>
+        <div className="flex h-[50px]">
+          <div
+            onClick={() => router.push("/shop/[user_id]/products")}
+            className={`${
+              routerType === "products"
+                ? "bg-white border-l border-t border-r border-[#212121]"
+                : "bg-[#fafafa] border-b border-[#212121]"
+            } flex-1 flex justify-center items-center text-sm cursor-pointer`}
+          >
+            상품
+          </div>
+          <div
+            onClick={() => router.push("/shop/[user_id]/reviews")}
+            className={`${
+              routerType === "reviews"
+                ? "bg-white border-l border-t border-r border-[#212121]"
+                : "bg-[#fafafa] border-b border-[#212121]"
+            } flex-1 flex justify-center items-center text-sm cursor-pointer`}
+          >
+            상점후기
+          </div>
+          <div
+            onClick={() => router.push("/shop/[user_id]/favorites")}
+            className={`${
+              routerType === "favorites"
+                ? "bg-white border-l border-t border-r border-[#212121]"
+                : "bg-[#fafafa] border-b border-[#212121]"
+            } flex-1 flex justify-center items-center text-sm cursor-pointer`}
+          >
+            찜
+          </div>
+          <div
+            onClick={() => router.push("/shop/[user_id]/followings")}
+            className={`${
+              routerType === "followings"
+                ? "bg-white border-l border-t border-r border-[#212121]"
+                : "bg-[#fafafa] border-b border-[#212121]"
+            } flex-1 flex justify-center items-center text-sm cursor-pointer`}
+          >
+            팔로잉
+          </div>
+          <div
+            onClick={() => router.push("/shop/[user_id]/followers")}
+            className={`${
+              routerType === "followers"
+                ? "bg-white border-l border-t border-r border-[#212121]"
+                : "bg-[#fafafa] border-b border-[#212121]"
+            } flex-1 flex justify-center items-center text-sm cursor-pointer`}
+          >
+            팔로워
+          </div>
+        </div>
+        <div className={`${routerType === "products" ? "" : "hidden"}`}>
+          <div className="text-lg pt-[50px] pb-[20px] border-b border-[#eeeeee] flex justify-between items-center">
+            <div>
+              상품
+              <span className="text-[#f72f33]">38</span>
+            </div>
+            <div
+              onMouseOver={() => setCategoriesFilter(true)}
+              onMouseOut={() => setCategoriesFilter(false)}
+              className="border-[1px] border-[#eeeeee] w-[140px] h-[28px] text-xs relative"
+            >
+              <div className="h-full pr-[10px] pl-[10px] flex items-center justify-between">
+                전체
+                <img
+                  src="/icons/down_arrow_category.png"
+                  width="10px"
+                  height="6px"
+                  alt="카테고리 화살표 아이콘"
+                />
+              </div>
+              <div
+                className={`${
+                  categoriesFilter ? "" : "hidden"
+                } absolute top-[27px] left-[-1px] w-[calc(100%+2px)] bg-white border-l border-b border-r border-[#eeeeee]`}
+              >
+                <div className="w-full h0[28px] px-[10px] leading-[28px] cursor-pointer">
+                  카테고리1
+                </div>
+                <div className="w-full h0[28px] px-[10px] leading-[28px] cursor-pointer">
+                  카테고리2
+                </div>
+                <div className="w-full h0[28px] px-[10px] leading-[28px] cursor-pointer">
+                  카테고리3
+                </div>
+                <div className="w-full h0[28px] px-[10px] leading-[28px] cursor-pointer">
+                  카테고리4
+                </div>
+              </div>
             </div>
           </div>
         </div>
