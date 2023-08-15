a915171fa441394eeb5b2f8550197e8497ec1aa9
limsongeun94 <cocorosaya@naver.com>
2023-08-15 13:41:01 +0900

    data

--- src/pages/api/landing.ts
+++ src/pages/api/landing.ts
@@ -1,28 +1,30 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import path from 'path'
-import { promises as fs } from 'fs'
+import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import { promises as fs } from "fs";
+import { data as categories } from "./categories";
+import { data as products } from "./index_products";
 
 // endpoint: /api/landing
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  
-  const dist = path.join(process.cwd(), 'data')
-  const products: Array<never> = JSON.parse(await fs.readFile(`${dist}/index_products.json`, 'utf8')).slice(0,52)
-  const categories = JSON.parse(await fs.readFile(`${dist}/categories.json`, 'utf8'))
+  // const dist = path.join(process.cwd(), 'data')
+  // const products: Array<never> = JSON.parse(await fs.readFile(`${dist}/index_products.json`, 'utf8')).slice(0,52)
+  // const categories = JSON.parse(await fs.readFile(`${dist}/categories.json`, 'utf8'))
+
   const ret = {
     banners: [
       {
-        url: '/banners/bag_banner.jpg'
+        url: "/banners/bag_banner.jpg",
       },
       {
-        url: '/banners/keyring_banner.jpg'
+        url: "/banners/keyring_banner.jpg",
       },
       {
-        url: '/banners/nuigurumi_banner.jpg'
+        url: "/banners/nuigurumi_banner.jpg",
       },
     ],
     products,
-    categories
-  }
+    categories,
+  };
 
   res.status(200).json(ret);
 };
--- src/pages/api/product.ts
+++ src/pages/api/product.ts
@@ -1,21 +1,20 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import path from 'path'
-import { promises as fs } from 'fs'
-
+import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import { promises as fs } from "fs";
+import { data } from "./index_products";
 // endpoint: /api/product
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const page = parseInt(req.query.page as string);
+  const size = parseInt(req.query.size as string);
 
-  const page = parseInt(req.query.page as string)
-  const size = parseInt(req.query.size as string)
-  
-  const dist = path.join(process.cwd(), 'data')
-  const data: Array<never> = JSON.parse(await fs.readFile(`${dist}/index_products.json`, 'utf8'))
+  // const dist = path.join(process.cwd(), 'data')
+  // const data: Array<never> = JSON.parse(await fs.readFile(`${dist}/index_products.json`, 'utf8'))
 
   const ret = {
     page,
     size,
     list: data.slice((page - 1) * size, page * size),
-  }
+  };
 
   res.status(200).json(ret);
 };
