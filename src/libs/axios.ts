c2d861ebd85588ad40220e5043a5eb46b01ffeb0
limsongeun94 <cocorosaya@naver.com>
2023-08-15 13:28:54 +0900

    Update axios.ts

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -2,6 +2,6 @@ import Axios from "axios";
 
 export default Axios.create({
   baseURL: "http://localhost:3000/api",
-  timeout: 1000,
+  // timeout: 1000,
   // headers: {'Content-Type': 'application/json'}
 });
