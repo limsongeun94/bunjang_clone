38456a6662877b4a12d0e887647079426d885250
limsongeun94 <cocorosaya@naver.com>
2023-08-15 13:58:53 +0900

    dotenv

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,9 @@
 import Axios from "axios";
 
+console.log("process.env.API_HOST", process.env.API_HOST);
+
 export default Axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.API_HOST,
   // timeout: 1000,
   // headers: {'Content-Type': 'application/json'}
 });
