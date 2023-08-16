1947c1b3bd4bff18bc56799720027f4ae7b986d5
limsongeun94 <cocorosaya@naver.com>
2023-08-16 19:59:42 +0900

    ....

--- src/components/Pagenation.tsx
+++ src/components/Pagenation.tsx
@@ -1,23 +1,32 @@
+import { useEffect, useState } from "react";
+
 type IndexProps = {
   totalPageCnt: number;
 };
 
 export default (props: IndexProps) => {
   // 1부터 10까지의 배열
-  const tenPageArr = new Array(10).fill().map((el, i) => {
+  const tenPageArr: number[] = new Array(10).fill().map((el, i) => {
     return i + 1;
   });
   // 총 페이지수 까지의 숫자로 이루어진 배열
-  const totalPageArr = new Array(props.totalPageCnt).fill().map((el, i) => {
-    return i + 1;
-  });
+  const totalPageArr: number[] = new Array(props.totalPageCnt)
+    .fill()
+    .map((el, i) => {
+      return i + 1;
+    });
 
-  console.log(totalPageArr);
+  // 현재 페이지에 표시되는 페이지 버튼 숫자의 배열
+  const [currentPageBtn, setCurrentPageBtn] = useState<number[]>(
+    props.totalPageCnt > 10 ? tenPageArr : totalPageArr
+  );
+
+  console.log(currentPageBtn);
 
   return (
     <div id="pagenation" className="pb-[100px]">
       <div className="flex justify-center">
-        {props.totalPageCnt > 10
+        {/* {props.totalPageCnt > 10
           ? tenPageArr.map((el, i) => {
               return <button key={i}>{el}</button>;
             })
@@ -27,7 +36,14 @@ export default (props: IndexProps) => {
                   {el}
                 </button>
               );
-            })}
+            })} */}
+        {currentPageBtn.map((el, i) => {
+          return (
+            <button key={i} className="pageBtn">
+              {el}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
