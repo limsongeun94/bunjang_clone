7742d49f2e3c28b5ed2749bf593cd30eae034d34
limsongeun94 <cocorosaya@naver.com>
2023-09-14 17:52:57 +0900

    로그인 정보 기재

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -17,44 +17,60 @@ export default () => {
 
   return (
     <div className="bg-[#f9f9f9] py-[100px]">
-      <form
-        action="/api/login"
-        method="POST"
-        className="bg-white m-auto w-[450px] p-[60px] rounded-md shadow-[0px_3px_6px_rgba(0,0,0,0.1)] box-content"
-      >
-        <h1 className="font-black text-3xl text-[#3f3f3f] mb-[45px]">
-          로그인 정보를 입력해주세요
-        </h1>
-        <MyInput
-          placeholder={"예시: bunjang@gmail.com"}
-          inputType={"text"}
-          name={"email"}
-          onChange={(value: string): void => {
-            setInfo({ ...info, email: value });
-          }}
+      <div className="bg-white mx-auto mb-[30px] p-[60px]  w-[570px] rounded-md shadow-[0px_3px_6px_rgba(0,0,0,0.1)] ">
+        <div className="grid grid-cols-2 w-[570px] grid-rows-2">
+          <div className="col-span-2 text-xl font-bold">로그인 정보</div>
+          <div className="text-sm">
+            email : seoly0173@naver.com <br />
+            password : qwer123!
+          </div>
+          <div className="text-sm">
+            email : coco@naver.com <br />
+            password : qwer123!
+          </div>
+        </div>
+      </div>
+      <div>
+        <form
+          action="/api/login"
+          method="POST"
+          className="bg-white m-auto w-[450px] p-[60px] rounded-md shadow-[0px_3px_6px_rgba(0,0,0,0.1)] box-content"
         >
-          이메일
-        </MyInput>
-        <MyInput
-          placeholder={"예시: psword098!!"}
-          inputType={"password"}
-          name={"password"}
-          onChange={(value: string): void => {
-            setInfo({ ...info, password: value });
-          }}
-        >
-          비밀번호
-        </MyInput>
-        <button
-          type="submit"
-          disabled={activeBtnClass ? false : true}
-          className={`${
-            activeBtnClass ? "" : "opacity-30 cursor-not-allowed"
-          } bg-[#d80c18] w-full h-[72px] rounded-[6px] text-lg text-white text-center`}
-        >
-          제출
-        </button>
-      </form>
+          <h1 className="font-black text-3xl text-[#3f3f3f] mb-[45px]">
+            로그인 정보를 입력해주세요
+          </h1>
+
+          <MyInput
+            placeholder={"예시: bunjang@gmail.com"}
+            inputType={"text"}
+            name={"email"}
+            onChange={(value: string): void => {
+              setInfo({ ...info, email: value });
+            }}
+          >
+            이메일
+          </MyInput>
+          <MyInput
+            placeholder={"예시: psword098!!"}
+            inputType={"password"}
+            name={"password"}
+            onChange={(value: string): void => {
+              setInfo({ ...info, password: value });
+            }}
+          >
+            비밀번호
+          </MyInput>
+          <button
+            type="submit"
+            disabled={activeBtnClass ? false : true}
+            className={`${
+              activeBtnClass ? "" : "opacity-30 cursor-not-allowed"
+            } bg-[#d80c18] w-full h-[72px] rounded-[6px] text-lg text-white text-center`}
+          >
+            제출
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
